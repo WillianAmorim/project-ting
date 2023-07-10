85fea5959faa4b5ad967be22cc1b66ae7e11127d
Willian <willian.amorim1@outlook.com>
2023-07-09 23:40:50 -0300

    resolvido requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,3 +22,22 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+    for index in range(0, len(instance)):
+        file_data = instance.search(index)
+        file_name = file_data["nome_do_arquivo"]
+        file_lines = file_data["linhas_do_arquivo"]
+        occurrences = []
+
+        for line, content in enumerate(file_lines, 1):
+            if word.lower() in content.lower():
+                occurrences.append({"linha": line, "conteudo": content})
+        
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            })
+
+    return result
